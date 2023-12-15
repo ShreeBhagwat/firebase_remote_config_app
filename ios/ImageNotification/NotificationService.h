1fe89b93c28b238848678d6979a27d70dce44db8
Shree Bhagwat <shree@codeaamy.com>
2023-12-15 19:17:14 +0530

    imagenotification extension

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Shree Bhagwat on 15/12/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
